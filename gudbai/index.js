8dbc2cb559485afcb65fc138f25a8a0bc61001e9
Víctor <muzik1421@gmail.com>
2019-03-12 13:13:47 -0600

    Created status codes for API responses, working successful.

--- gudbai/index.js
+++ gudbai/index.js
@@ -45,17 +45,17 @@ router.get('/summonerName/:summoner', function(req, res)
 	// LET CHECK
 	var summoner = req.params.summoner;
 	
-	while(summoner.charAt(0) === '0')
-	{
- 		summoner = summoner.substr(1);
-	}
-	console.log(summoner);
+	//console.log(summoner);
 	
 	// TO DO
 	/* Something */
 	var json_response;
 	
-	var apiKey = 'RGAPI-0af1caff-443d-4528-894a-7d99048d42dc';
+	//var apiKey = 'RGAPI-0af1caff-443d-4528-894a-7d99048d42dc';
+	var apiKey = 'RGAPI-5839053f-5ce9-4a66-bad3-5a643e0b633c';
+	
+	var response_status;
+	
 	https.get
 	(
 		'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summoner +'?api_key=' + apiKey, 
@@ -73,8 +73,11 @@ router.get('/summonerName/:summoner', function(req, res)
 					
 					
   				}
-			);/*
-			res.on("close", function()
+			);
+			
+			response_status = res.statusCode;
+			//console.log(response_status);
+			/*res.on("close", function()
 			{
         		console.log('termino xd');
 				//console.log(jsonObject);
@@ -87,7 +90,7 @@ router.get('/summonerName/:summoner', function(req, res)
 		'error', 
 		(e) =>
 		{
-  			console.error(e);
+			flag = false;
 		}
 	)
 	.on
@@ -95,27 +98,76 @@ router.get('/summonerName/:summoner', function(req, res)
 		"close",
 		function()
 		{
-    		console.log('termino xd');
+    		//console.log('termino xd');
 			//console.log(json_response.summonerLevel);
-			//console.log(jsonObject);
+			//console.log(json_response);
 			//return jsonObject;
-			res.json
-			(
-				{
-					//summoner: req.params
-					summoner: json_response.name,
-					profileIcon: json_response.profileIconId,
-					summonerLevel: json_response.summonerLevel,
-					accountID: json_response.accountId,
-					summonerID: json_response.id
-				}
-			);
+			
+			//console.log(res.statusCode);
+			
+			//TO RESPONSE
+			
+			
+			if(response_status == 200)
+			{
+				res.json
+				(
+					{
+						status: response_status,
+						summoner: json_response.name,
+						profileIcon: json_response.profileIconId,
+						summonerLevel: json_response.summonerLevel,
+						accountID: json_response.accountId,
+						summonerID: json_response.id
+					}
+				);
+			}
+			
+			else if(response_status == 403)
+			{
+				res.json
+				(
+					{
+						status: response_status,
+						message: 'The API key has expired!'
+					}
+				);
+			}
+			
+			else if(response_status == 404)
+			{
+				res.json
+				(
+					{
+						status: response_status,
+						message: 'We couldnt find a summoner.'
+					}
+				);
+			}
+			
+			else if(response_status == 500)
+			{
+				res.json
+				(
+					{
+						status: response_status,
+						message: 'Problems with Riot Services.'
+					}
+				);
+			}
+			
+			else
+			{
+				res.json
+				(
+					{
+						status: response_status,
+						message: 'An error has occurred, code:' + response_status
+					}
+				);
+			}
     	}
-	);
-	
-	//TO RESPONSE
-	
-	
+	);	
 });
 
 router.get('/elo/:summoner', function(req, res)
